c0819140915ba428470539cc6482f75cd297f865
Igor Tamara <igor@axiacore.com>
2014-04-16 21:36:25 -0500

    Added first word to the plate

--- games/soupletter.js
+++ games/soupletter.js
@@ -4,6 +4,17 @@ function SoupLetter(incoming_words){
     self.local_matrix = undefined;
     self.words = incoming_words.split(',');
     self.local_matrix = new Array();
+    self.positions = new Array();
+
+    self.cleanmatrix = function(cant){
+        self.local_matrix = new Array();
+        for (i = 0; i < cant; i++) {
+            self.local_matrix[i] = new Array();
+            for (j = 0; j< cant; j++){
+                self.local_matrix[i][j] = '-';
+            }
+        }
+    }
 
     self.calculatepuzzle = function() {
         max_length = self.words[0].length;
@@ -15,15 +26,9 @@ function SoupLetter(incoming_words){
             cant += self.words[i].length;
         }
         cant = Math.max(Math.ceil(Math.sqrt(cant)), max_length) + 3;
-        self.local_matrix = new Array();
-        for (i = 0; i < cant; i++) {
-            self.local_matrix[i] = new Array();
-            for (j = 0; j< cant; j++){
-                self.local_matrix[i][j] = '';
-            }
-        }
-        console.log(self.local_matrix);
+        self.cleanmatrix(cant);
     };
+
     self.putwords = function (){
         var clone = self.words.slice(0);
         clone.sort
@@ -35,8 +40,20 @@ function SoupLetter(incoming_words){
         var word = clone[0];
         var x = 1;
         var y = Math.min(Math.floor(Math.random() * self.local_matrix.length), self.local_matrix.length -1);
-        console.log(y)
+        for (i=0; i < clone[0].length; i++){
+            self.local_matrix[x + i][y] = clone[0][i];
+        }
+        self.positions.push({'i': x, 'j': y, 'x': 1, 'y': 0})
     }
+
+    self.showmatrix = function(){
+        joined = ""
+        for (j = 0; j < self.local_matrix.length; j++){
+            joined += self.local_matrix[j].join("") + "\n";
+        }
+        return joined;
+    }
+
     self.calculatepuzzle();
     self.putwords();
 }
