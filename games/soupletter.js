f541e13f89bebea590983784744e0890852ce0d3
Igor Tamara <igor@axiacore.com>
2014-04-20 01:12:41 -0500

    Added fill function

--- games/soupletter.js
+++ games/soupletter.js
@@ -1,16 +1,17 @@
 function SoupLetter(incoming_words){
 
     var self = this;
-    self.local_matrix = undefined;
-    self.words = incoming_words.split(',');
-    self.local_matrix = new Array();
-    self.positions = new Array();
+    self.words = incoming_words.split(','); // Holds the incoming words
+    self.local_matrix = new Array(); // Holds the board of the soup letter
+    self.positions = new Array(); // Holds the positions of the words 
     self.getsin = [
         function(x, k, length, max){ return x - k >= 0 && x + length - k < max; },
         function(x, k, length, max){ return x - length + k >= 0 && x + k < max;},
     ];
+    self.letters = new Array();  // Holds the letters of the incoming words
 
     self.cleanmatrix = function(cant){
+        // Initializes a matrix of size cant * cant with dashes
         self.local_matrix = new Array();
         for (var i = 0; i < cant; i++) {
             self.local_matrix[i] = new Array();
@@ -20,21 +21,11 @@ function SoupLetter(incoming_words){
         }
     }
 
-    self.calculatepuzzle = function() {
-        var max_length = self.words[0].length;
-        var cant = max_length;
-        for (var i = 1; i < self.words.length; i++) {
-            if (max_length < self.words[i].length){
-                max_length = self.words[i].length;
-            }
-            cant += self.words[i].length;
-        }
-        cant = Math.max(Math.ceil(Math.sqrt(cant)), max_length) + 3;
-        self.cleanmatrix(cant);
-    };
-
-
     self.try_word = function(x_dir, y_dir, p_x, p_y, k, word, max){
+        // Tries to put a word in direction x_dir, y_dir, with the intersection
+        // located at p_x, p_y with the letter at position k of the word
+        // it returns false if the word is out of bounds or is no room because
+        // there are other words that do not allow to put the word properly 
         var w_length = word.length;
         var good = false;
         var x_ini = p_x;
@@ -100,7 +91,8 @@ function SoupLetter(incoming_words){
     };
 
     self.put_word = function(dir_x, dir_y, p_x, p_y, word){
-        for(var i=0; i < word.length; i++){
+        // Puts the word in direction dir_x, dir_y starting at the point p_x, p_y
+        for(i=0; i < word.length; i++){
             self.local_matrix[p_x + (i * dir_x)][p_y + (i * dir_y)] = word[i];
         }
         self.positions[word] = ({'i': p_x, 'j': p_y, 'x': dir_x, 'y': dir_y})
@@ -134,7 +126,18 @@ function SoupLetter(incoming_words){
         return false;
     }
 
+    self.fill_empties = function(){
+        for (var i = 0; i < self.local_matrix.length; i++){
+            for (var j = 0; j < self.local_matrix.length; j++){
+                if (self.local_matrix[i][j] === '-') {
+                    self.local_matrix[i][j] = self.letters[Math.floor(Math.random() * self.letters.length)];
+                }
+            }
+        }
+    }
+
     self.put_words = function (){
+        // fills the board with the hidden words
         var clone = self.words.slice(0);
         clone.sort
         (
@@ -143,27 +146,85 @@ function SoupLetter(incoming_words){
             }
         );
         var word = clone[0];
-        var x = 1;
+        var half_word = Math.floor(clone[0].length / 2);
+        var x = Math.floor(self.local_matrix.length / 2);
         var y = Math.min(Math.floor(Math.random() * self.local_matrix.length), self.local_matrix.length -1);
         var cur_idx = 0;
         var others = new Array();
         var found = false;
-        var dir_x = Math.floor(Math.random() * 3) - 1;
-        var dir_y = Math.floor(Math.random() * 3) - 1;
-        self.try_put_word(word, 0, 0, {'i': x, 'j': y, 'x': dir_x, 'y': dir_y});
+        var l = 0;
+        self.try_put_word(word, 0, half_word, {'i': x, 'j': y, 'x': 0, 'y': 1});
+        for (var w = 0; w < clone.length; w++) {
+            for (var h = 0; h < clone[w].length; h++){
+                self.letters.push(clone[w][h]);
+            }
+        }
+        for (var j = 1; j < clone.length; j++){
+            // j loops over the words we want to include in the board
+            positioned = false;
+            for (word in self.positions){
+                // Loop over the previous reviewed words
+                for(var k = 0; k < word.length; k++){
+                    // k loops over the letters on the word to be included
+                    l = clone[j].indexOf(word[k], 0);
+                    // l is used to determine if both words intersect
+                    while(l != -1){
+                        // We try to position the word in the intersection
+                        if (!self.try_put_word(clone[j], k, l, self.positions[word])){
+                            l = clone[j].indexOf(word[k], l + 1);
+                        }
+                        else {
+                            positioned = true
+                            l = -1;
+                            k = word.length;
+                        }
+                    }
+                }
+                if (positioned){
+                    break;
+                }
+            }
+            if (!positioned){
+                var max_half = Math.floor(self.local_matrix.length / 2);
+                half_word = Math.floor(clone[j].length / 2);
+                for(var m = 0; !positioned  && m < max_half; m++) {
+                    for (var n = 0; !positioned && n < max_half; n++) {
+                        var place = {'i': m, 'j': n, 'x': 0, 'y': 1};
+                        positioned = self.try_put_word(clone[j], 0, half_word, place);
+                        if (!positioned){
+                            place = {'i': max_half - m, 'j': max_half - n, 'x': 0, 'y': 1};
+                            positioned = self.try_put_word(clone[j], 0, half_word, place);
+                        }
+                    }
+                }
+            }
+        }
+        self.fill_empties();
     }
 
-    self.showmatrix = function(){
+    self.calculatepuzzle = function() {
+        // Creates a matrix that should have enough room for all the words contained in self.words
+        var max_length = self.words[0].length;
+        var cant = max_length;
+        for (var i = 1; i < self.words.length; i++) {
+            if (max_length < self.words[i].length){
+                max_length = self.words[i].length;
+            }
+            cant += self.words[i].length;
+        }
+        cant = Math.max(Math.ceil(Math.sqrt(cant)), max_length) + 3;
+        self.cleanmatrix(cant);
+        self.put_words();
+    };
+
+    self.show_plain_matrix = function(){
+        // Shows a plain matrix
         joined = ""
         for (j = 0; j < self.local_matrix.length; j++){
             joined += self.local_matrix[j].join("") + "\n";
         }
         return joined;
+
     }
-
     self.calculatepuzzle();
-    self.put_words();
-    self.showmatrix();
 }
-
-a = new SoupLetter('gato,perro,canario,elefante');
