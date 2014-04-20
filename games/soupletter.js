64324ceb23d97e994b718652860e8a6288379aed
Igor Tamara <igor@axiacore.com>
2014-04-19 21:33:19 -0500

    Added word positioning

--- games/soupletter.js
+++ games/soupletter.js
@@ -5,21 +5,25 @@ function SoupLetter(incoming_words){
     self.words = incoming_words.split(',');
     self.local_matrix = new Array();
     self.positions = new Array();
+    self.getsin = [
+        function(x, k, length, max){ return x - k >= 0 && x + length - k < max; },
+        function(x, k, length, max){ return x - length + k >= 0 && x + k < max;},
+    ];
 
     self.cleanmatrix = function(cant){
         self.local_matrix = new Array();
-        for (i = 0; i < cant; i++) {
+        for (var i = 0; i < cant; i++) {
             self.local_matrix[i] = new Array();
-            for (j = 0; j< cant; j++){
+            for (var j = 0; j< cant; j++){
                 self.local_matrix[i][j] = '-';
             }
         }
     }
 
     self.calculatepuzzle = function() {
-        max_length = self.words[0].length;
-        cant = max_length;
-        for (i = 1; i < self.words.length; i++) {
+        var max_length = self.words[0].length;
+        var cant = max_length;
+        for (var i = 1; i < self.words.length; i++) {
             if (max_length < self.words[i].length){
                 max_length = self.words[i].length;
             }
@@ -29,7 +33,108 @@ function SoupLetter(incoming_words){
         self.cleanmatrix(cant);
     };
 
-    self.putwords = function (){
+
+    self.try_word = function(x_dir, y_dir, p_x, p_y, k, word, max){
+        var w_length = word.length;
+        var good = false;
+        var x_ini = p_x;
+        var y_ini = p_y;
+        var target = 0;
+        if (x_dir > 0 && self.getsin[0](p_x, k, w_length, max)) {
+            x_ini -= k;
+            if(y_dir > 0 && self.getsin[0](p_y, k, w_length, max)) {
+                // 1 1
+                y_ini -= k;
+                good = true;
+            }
+            if (y_dir < 0 && self.getsin[1](p_y, k, w_length, max)) {
+                // 1 -1
+                y_ini += k;
+                good = true;
+            }
+            if (y_dir === 0) {
+                // 1 0
+                good = true;
+            }
+        }
+        else if(x_dir < 0 && self.getsin[1](p_x, k, w_length, max)){
+            x_ini += k;
+            if (y_dir > 0 && self.getsin[0](p_y, k, w_length, max)) {
+                // -1 1
+                y_ini -= k;
+                good = true;
+            }
+            if (y_dir < 0 && self.getsin[1](p_y, k, w_length, max)) {
+                // -1 -1
+                y_ini += k;
+                good = true;
+            }
+            if (y_dir === 0){
+                // -1 0
+                good = true;
+            }
+        }
+        else if (x_dir === 0){
+            if (y_dir > 0 && self.getsin[0](p_y, k, w_length, max)) {
+                // 0 1
+                y_ini -= k;
+                good = true;
+            }
+            if (y_dir < 0 && self.getsin[1](p_y, k, w_length, max)) {
+                // 0 -1
+                y_ini +=k;
+                good = true;
+            }
+        }
+        if (!good){
+            return false;
+        }
+        for(var i = 0; i < w_length; i++) {
+            target = self.local_matrix[x_ini + (i * x_dir)][y_ini + (i * y_dir)];
+            if(target != '-' && target != word[i]) {
+                return false;
+            }
+        }
+        self.put_word(x_dir, y_dir, x_ini, y_ini, word);
+        return true;
+    };
+
+    self.put_word = function(dir_x, dir_y, p_x, p_y, word){
+        for(var i=0; i < word.length; i++){
+            self.local_matrix[p_x + (i * dir_x)][p_y + (i * dir_y)] = word[i];
+        }
+        self.positions[word] = ({'i': p_x, 'j': p_y, 'x': dir_x, 'y': dir_y})
+    }
+
+    self.try_put_word = function(cur_word, inter_word, inter_cur, position){
+        // Determines if the cur can be added given 
+        // Positions the word if possible
+        // cur_word word to be positioned
+        // inter_word: character position of intersection of the positioned word
+        // inter_cur: character position of intersection of the target word
+        // position: information of the current word inside the board
+        var p_x = position['i'] + inter_word * position['x'];
+        var p_y = position['j'] + inter_word * position['y'];
+        var max = self.local_matrix.length;
+        var tries = [
+            [1, 0],
+            [-1, 0],
+            [0, 1],
+            [0, -1],
+            [1, 1],
+            [1, -1],
+            [-1, 1],
+            [-1, -1],
+        ]
+        for (var i = 0; i < 8; i++){
+            if(self.try_word(tries[i][0], tries[i][1], p_x, p_y, inter_cur, cur_word, max)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    self.put_words = function (){
         var clone = self.words.slice(0);
         clone.sort
         (
@@ -40,10 +145,12 @@ function SoupLetter(incoming_words){
         var word = clone[0];
         var x = 1;
         var y = Math.min(Math.floor(Math.random() * self.local_matrix.length), self.local_matrix.length -1);
-        for (i=0; i < clone[0].length; i++){
-            self.local_matrix[x + i][y] = clone[0][i];
-        }
-        self.positions.push({'i': x, 'j': y, 'x': 1, 'y': 0})
+        var cur_idx = 0;
+        var others = new Array();
+        var found = false;
+        var dir_x = Math.floor(Math.random() * 3) - 1;
+        var dir_y = Math.floor(Math.random() * 3) - 1;
+        self.try_put_word(word, 0, 0, {'i': x, 'j': y, 'x': dir_x, 'y': dir_y});
     }
 
     self.showmatrix = function(){
@@ -55,7 +162,8 @@ function SoupLetter(incoming_words){
     }
 
     self.calculatepuzzle();
-    self.putwords();
+    self.put_words();
+    self.showmatrix();
 }
 
 a = new SoupLetter('gato,perro,canario,elefante');
